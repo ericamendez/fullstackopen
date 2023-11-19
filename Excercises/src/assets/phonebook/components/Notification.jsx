7590b6bdf4ed15b8df1bd3e0d5c2e04c61d89714
Erica Mendez <ericamendez@Ericas-MacBook-Air.local>
2023-11-19 02:07:20 -0500

    quick

--- Excercises/src/assets/phonebook/components/Notification.jsx
+++ Excercises/src/assets/phonebook/components/Notification.jsx
@@ -1,10 +1,10 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, isError }) => {
     if (message === null) {
       return null
     }
   
     return (
-      <div className='error'>
+      <div className={isError ? 'error': 'success'}>
         {message}
       </div>
     )
